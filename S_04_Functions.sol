36461dd96a0225674cbdb75e0a6289425ea6b6b8
jacky2code <jacky2code@gmail.com>
2022-04-22 14:28:57 +0800

    函数

--- S_04_Functions.sol
+++ S_04_Functions.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.7 <0.9.0;
  * 函数
  */
 contract Functions {
+    
     // external : 外部函数
     // pure : 纯函数，不能够读写状态变量，只能拥有局部变量
     function add(uint x, uint y) external pure returns (uint) {
