aac68d9a10745814581eaa70ea63d2baa947d18a
jacky2code <jacky2code@gmail.com>
2022-06-29 15:58:23 +0800

    事件

---
+++ S_27_Event.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+/**
+ * 事件：记录当前智能合约运行状态
+ * 时间存储更节约gas
+ */
+contract Event {
+    // 声明事件
+    event Log(string message, uint val);
+    // 带有索引的事件，最多不超过3个索引
+    event IndexedLog(address indexed sender, uint val);
+
+
+    function example() external {
+        // 触发事件
+        emit Log("foo", 123);
+        emit IndexedLog(msg.sender, 345);
+    }
+
+    event Message(address _from, address _to, string message);
+    
+    function sendMsg(address _to, string calldata message) external {
+        emit Message(msg.sender, _to, message);
+    }
+}
