15d0af03a0cc1f4dcb5b2f202241ed41e54cf94a
jacky2code <jacky2code@gmail.com>
2022-11-07 15:14:27 +0800

    Sample storage 简单存储

--- S_25_SimpleStorage.sol
+++ S_25_SimpleStorage.sol
@@ -1,19 +1,17 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-/**
- * 简单存储
- * - 
- */
+// 简单存储
 contract SimpleStorage {
     string public text;
 
-    // calldata 89626 gas
-    // memory 90114 gas
+    // 存储字符串：aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    // 使用 calldata 花费 89626 gas
+    // 使用 memory 花费 90114 gas
     function set(string calldata _text) external {
         text = _text;
     }
-
+    // 智能合约将状态变量拷贝到内存中再返回
     function get() external view returns (string memory) {
         return text;
     }
