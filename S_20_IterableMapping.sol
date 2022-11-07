4d26532ae90cb1766f7fe82455cdfc5cae56fa4e
jacky2code <jacky2code@gmail.com>
2022-11-07 10:53:53 +0800

    IterableMapping 可迭代映射

--- S_20_IterableMapping.sol
+++ S_20_IterableMapping.sol
@@ -1,50 +1,47 @@
-// SPDX-License-Identifier: GPL-2.0
-pragma solidity >=0.8.7 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
 /**
- *   IterableMapping
+ *   IterableMapping 可迭代映射
  */
 contract IterableMapping {
-    // 余额
-    mapping(address => uint256) public balances;
-    // 某地址是否存在映射中
-    mapping(address => bool) public inserted;
+    // 地址/余额
+    mapping(address => uint) public balances;
+    // 地址/是否存在
+    mapping(address => bool) public isInserted;
     // 所有存在地址
     address[] public keys;
 
     /** 添加地址 */
-    function set(address _key, uint256 _val) external {
+    function set(address _key, uint _val) external {
         // 给地址赋值余额
         balances[_key] = _val;
         // 判断地址是否在映射中
-        if (!inserted[_key]) {
+        if (!isInserted[_key]) {
             // 添加映射
-            inserted[_key] = true;
+            isInserted[_key] = true;
             // 添加存在地址
             keys.push(_key);
         }
     }
 
     /** 获取地址数组长度 */
-    function getSize() external view returns (uint256) {
+    function getSize() external view returns (uint) {
         return keys.length;
     }
 
     /** 获取第一个地址余额 */
-    function getFirstAddressBal() external view returns (uint256) {
+    function getFirstAddressBal() external view returns (uint) {
         return balances[keys[0]];
     }
 
     /** 获取最后一个地址的余额 */
-    function getLastAddressBal() external view returns (uint256) {
+    function getLastAddressBal() external view returns (uint) {
         return balances[keys[keys.length - 1]];
     }
 
     /** 获取任意位置地址的余额 */
-    function getAddressBalAtIndex(uint256 _index)
-        external
-        view
-        returns (uint256)
+    function getAddressBalAtIndex(uint _index) external view returns (uint)
     {
         return balances[keys[_index]];
     }
