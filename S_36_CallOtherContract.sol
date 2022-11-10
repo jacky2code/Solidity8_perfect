39c0a55cab2df9c54f7aab86f49442e9753c3836
jacky2code <jacky2code@gmail.com>
2022-11-10 10:17:56 +0800

    Call Contract 调用其他合约

--- S_36_CallOtherContract.sol
+++ S_36_CallOtherContract.sol
@@ -1,25 +1,28 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// 调用其他合约
 contract CallTestContract {
-    // function setX(address _test, uint _x) external {
-    //     TestContract(_test).setX(_x);
-    // }
 
-    function setX(TestContract _test, uint _x) external {
+    // set 方法1
+    function setX1(address _test, uint _x) external {
+        TestContract(_test).setX(_x);
+    }
+    // set 方法2
+    function setX2(TestContract _test, uint _x) external {
         _test.setX(_x);
     }
 
-    function getX (address _test) external view returns (uint){
-        return TestContract(_test).getX();
+    function getX (address _test) external view returns (uint x){
+        x = TestContract(_test).getX();
     }
 
     function setXandReceiveEther(address _test, uint _x) external payable {
         TestContract(_test).setXandReceiveEther{ value: msg.value }(_x);
     }
 
-    function getXandValue(address _test) external view returns (uint, uint) {
-        return TestContract(_test).getXandValue();
+    function getXandValue(address _test) external view returns (uint x, uint value) {
+        (x, value)= TestContract(_test).getXandValue();
     }
 }
 
